f6f48386ab260d97c5dd2313a0ce196aacb44f10
Roshan Sharma <reprogram46@gmail.com>
2020-07-22 13:05:27 +0530

    init: Set up app skeleton
    Only inserts available at this point.

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  devURI: 'mongodb://localhost:27017/employees',
+  PORT: 3000
+}
---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const uploader = require('express-fileupload')
+const { check, validationResult } = require('express-validator');
+const MongoClient = require('mongodb').MongoClient
+const { devURI, PORT } = require('./config')
+
+const app = express()
+app.use(express.urlencoded({extended: true}))
+app.use(uploader({
+  createParentPath: true
+}))
+
+let db
+MongoClient.connect(devURI, (err, client) => {
+  if (err) {
+    return console.error(`Cannot start the server: ${err}`)
+  }
+  db = client.db('employees')
+  app.listen(PORT,  () => {
+    console.log(`API running on ${PORT}...`)
+  })
+})
+
+
+// Default endpoint with default response
+app.get('/', (req, res) => {
+  res.sendStatus(200)
+})
+
+
+// Validate and save applicant details
+app.post('/hire',[
+  check('name').isLength({ min: 3 }),
+  check('email').isEmail(),
+], async (req, res) => {
+
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() })
+  }
+
+  try {
+    db.collection('hiring').save(req.body, (err, result) => {
+      if (err) {
+        throw err
+        return
+      }
+      console.log('Success! Data saved for candidate.')
+    })
+  }
+  catch (err) {
+    res.send(`Exception: ${err}`)
+  }
+})
